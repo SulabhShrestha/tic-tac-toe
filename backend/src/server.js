17732be1a71c0d0b73f94ee1a41ae0c071ee77e9
Sulabh Shrestha <shresthasulabh777@gmail.com>
2024-01-11 23:01:24 +0545

    Server socket created

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,7 +2,21 @@ const http = require("http");
 const express = require("express");
 const { Server } = require("socket.io");
 
+const app = express();
+
 const httpServer = http.createServer(app);
+
+const io = new Server(httpServer, {
+  serveClient: false,
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log(`a user connected ${socket.id}`);
+});
 
 httpServer.listen(3000, () => {
   console.log("Server is running on port 3000 ");
